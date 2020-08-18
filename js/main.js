3ba0102cee58d7c77339fdee3bb3e96570529f3c
wkk221 <wkk221@tutanota.com>
2020-08-18 08:57:44 +0800

    实现柱状图的绘制

--- js/main.js
+++ js/main.js
@@ -10,71 +10,90 @@ Mock.mock(baseURL + '/abc', {
 });
 
 // 
-(function(){
+(function () {
   var myChart = echarts.init(document.querySelector('.bar1 .chart'))
   var option = {
-      tooltip: {
-        trigger: 'axis',
-        showContent: true,
-        axisPointer: {
-          type: 'none',
-          type: 'shadow',
+    tooltip: {
+      trigger: 'axis',
+      showContent: true,
+      axisPointer: {
+        type: 'shadow',
+      }
+    },
+
+    yAxis: {
+      type: 'value',
+      axisLabel: {
+        color: 'rgba(225,225,225.6)',
+        fontSize: 10
+      },
+      splitLine: {
+        show: true,
+        lineStyle: {
+          color: "rgba(255,255,255,.2)",
+          width: 1
         }
       },
-      xAxis: {
-          type: 'category',
-          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-          axisLabel: {
-            color: '#fff'
-          },
-          axisPointer: {
-            show: true
-          },
-          axisLine: {
-            show: false
-          }
+      axisTick: { show: false }
+    },
+    xAxis: {
+      type: 'category',
+      data: ['旅游行业', '教育培训', '游戏行业', '医疗行业', '电商行业', '社交行业', '金融行业'],
+      axisLabel: {
+        color: 'rgba(225,225,225,.6)',
+        fontSize: 10
       },
-      yAxis: {
-          type: 'value',
-          axisLabel: {
-            color: '#fff'
-          },
-          splitLine: {
-            lineStyle: {
-              color: "rgba(241, 232, 232, .3)",
-              width: 0.5
-            }
-          }
+      axisPointer: {
+        show: true
       },
-      grid: {
-          show: true,
-          left: '12%',
-          top: '5%',
-          right: '0',
-          bottom: '12%',
-          containLabel: false
+      axisTick: { show: false },
+      // 画布刻度线主线。
+      axisLine: {
+        show: true,
+        lineStyle: {
+          color: 'red',
+          width: 1,
+        }
       },
-      series: [{
-        color: ['rgb(75, 127, 214)'],
-          data: [],
-          type: 'bar',
-          barWidth: 25,
-          itemStyle: {
-            barBorderRadius: 5
-          },
-      }]
+      splitLine: {
+        show: false
+      }
+    },
+    grid: {
+      show: true,
+      left: '10%',
+      top: '12%',
+      right: '0',
+      bottom: '12%',
+      containLabel: false
+    },
+    series: [{
+      color: ['rgb(75, 127, 214)'],
+      data: [],
+      type: 'bar',
+      barWidth: '35%',
+      itemStyle: {
+        barBorderRadius: 5
+      }
+    }]
   }
 
-  $http.get('/bar1').then(function(res) {
+  $http.get('/bar1').then(function (res) {
     console.log(res)
     var result = res.data.data
-    option.series[0].data = result
+    // option.series[0].data = result
+    option.series[0].data = [200, 300, 300, 900, 1500, 1200, 600]
     myChart.setOption(option)
     //myChart2.setOption(option)
   })
+
+  // 浏览器发生调整
+  window.addEventListener('resize', function () {
+    myChart.resize()
+  })
 })();
 
-(function(){
+(function () {
   var colors = [
     'rgb(54, 137, 230)',
     'rgb(237, 113, 114)',
@@ -83,57 +102,80 @@ Mock.mock(baseURL + '/abc', {
     'rgb(140, 118, 244)']
   var myChart = echarts.init(document.querySelector('.bar2 .chart'))
   var option = {
-    tooltip: {},
-    legend: {
-      show: false
-    },
+
     grid: {
-      top: 25,
-      left: '15%',
+      left: '22%',
+      // right: '4%',
       bottom: '10%',
-      right: '3%'
-    },
-    yAxis: {
-      data: ["HTML5", "CSS3", "JAVASCRIPT", "VUE", "NODE"],
-      axisLine: {
-        show: false,
-      },
-      axisTick: {
-        show: false
-      },
-      splitLine: {
-        show: false
-      },
-      axisLabel: {
-        color: '#fff'
-      }
+      top: '10%',
+      containLabel: false
     },
     xAxis: {
-      show: false
+      show: false,
+      type: 'value',
+      
     },
-    series: [{
-      name: "Sale",
-      type: "bar",
-      data: [5, 20, 36, 10, 10],
-      showBackground: true,
-      barWidth: 8,
-      backgroundStyle: {
-        color: "rgba(237, 193, 193, 0)",
-        borderColor: "rgba(65, 194, 222, 1)",
-        borderWidth: 2,
-        barBorderRadius: [40, 40, 40, 40],
+    yAxis: [
+      {
+        inverse: true,
+        axisTick: { show:false},
+        axisLine: { show: false},
+        axisLabel: { color: '#fff' },
+        type: 'category',
+        data: ['html5','css3','javascript','vue','node']
       },
-      itemStyle: {
-        color: function(item){
-          return colors[item.dataIndex] 
+      {
+        inverse: true,
+        axisTick: { show:false},
+        axisLine: { show: false},
+        axisLabel: { color: '#fff' },
+        type: 'category',
+        data: [702, 350, 610, 793, 664]
+      }
+    ],
+    series: [
+      {
+        name: '条',
+        type: 'bar',
+        yAxisIndex: 0,
+        data: [70, 34, 60, 78, 69],
+        itemStyle: {
+          color: function(item){
+            return colors[item.dataIndex %colors.length]
+          },
+          barBorderRadius: 20,
         },
-        barBorderRadius: [15, 15, 15, 15]
+        barWidth: 10,
+        barCategoryGap: 10,
+        label: {
+          show: true,
+          position: 'inside',
+          formatter: '{c}%'
+        }
       },
-    }]
-  }
+      {
+        name: '框',
+        type: 'bar',
+        yAxisIndex: 1,
+        data: [100, 100, 100, 100, 100],
+        itemStyle: {
+          barBorderRadius: 20,
+          color: 'transparent',
+          borderWidth: 3,
+          borderColor: 'rgba(65, 194, 222,1)'
+        },
+        barWidth: 15,
+        barCategoryGap: 50,
+      }
+    ]
+  };
   myChart.setOption(option)
+  // 浏览器发生调整
+  window.addEventListener('resize', function () {
+    myChart.resize()
+  })
 })();
 
-$http.get('/abc').then(function(res) {
+$http.get('/abc').then(function (res) {
   console.log(res)
 })
