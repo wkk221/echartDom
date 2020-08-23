e9d34ad4e7c004c53988ab1add357646fdf849e9
wkk221 <wkk221@tutanota.com>
2020-08-23 13:16:28 +0800

    折线图的实现

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ Mock.mock(baseURL + '/abc', {
   'data|7': [/[1-9][0-9]{3}/]
 });
 
-// 
+// 柱状图1
 (function () {
   var myChart = echarts.init(document.querySelector('.bar1 .chart'))
   var option = {
@@ -18,7 +18,8 @@ Mock.mock(baseURL + '/abc', {
       showContent: true,
       axisPointer: {
         type: 'shadow',
-      }
+      },
+      extraCssText:'background: pink;color:red;'
     },
 
     yAxis: {
@@ -60,7 +61,7 @@ Mock.mock(baseURL + '/abc', {
       }
     },
     grid: {
-      show: true,
+      show: false,
       left: '10%',
       top: '12%',
       right: '0',
@@ -93,6 +94,7 @@ Mock.mock(baseURL + '/abc', {
   })
 })();
 
+// 柱状图2
 (function () {
   var colors = [
     'rgb(54, 137, 230)',
@@ -101,6 +103,7 @@ Mock.mock(baseURL + '/abc', {
     'rgb(242, 179, 74)',
     'rgb(140, 118, 244)']
   var myChart = echarts.init(document.querySelector('.bar2 .chart'))
+  
   var option = {
 
     grid: {
@@ -151,6 +154,11 @@ Mock.mock(baseURL + '/abc', {
           show: true,
           position: 'inside',
           formatter: '{c}%'
+        },
+        animationEasing: 'backOut',
+        animationDelay: function(index) {
+          console.log(index)
+          return index * 50
         }
       },
       {
@@ -166,6 +174,7 @@ Mock.mock(baseURL + '/abc', {
         },
         barWidth: 15,
         barCategoryGap: 50,
+        animationDuration: 0
       }
     ]
   };
@@ -176,6 +185,196 @@ Mock.mock(baseURL + '/abc', {
   })
 })();
 
-$http.get('/abc').then(function (res) {
-  console.log(res)
-})
+// 折线图1
+(function(){
+  var myChart = echarts.init(document.querySelector('.line1 .chart'))
+  var ajaxData =  [
+    [Mock.mock({'data|12':[/^[1-2]\d{1,2}/]}).data, Mock.mock({'data|12':[/^[1-2]\d{1,2}/]}).data],
+    [Mock.mock({'data|12':[/^[1-2]\d{1,2}/]}).data, Mock.mock({'data|12':[/^[1-2]\d{1,2}/]}).data]
+  ]
+  var option = {
+    color: ['#00f1f2','#ed3f35'],
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        // data: ['新增粉丝','新增游客'],
+        textStyle: {
+          color: '#4c9bfd'
+        },
+        right: '5%'
+    },
+    grid: {
+        top:'20%',
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true,
+        show: true,
+        borderColor: 'rgb(38, 42, 79)'
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
+        axisTick: {show: false},
+        splitLine: {show: false},
+        axisLine: { show: false},
+        axisLabel: {textStyle: {color: '#4c9bfd'}}
+    },
+    yAxis: {
+        type: 'value',
+        splitLine: {lineStyle: {color: '#012f4a'}},
+        axisTick: {show: false},
+        axisLine: { show: false},
+        axisLabel: {textStyle: {color: '#4c9bfd'}}
+    },
+    series: [
+        {
+            name: '新增粉丝',
+            type: 'line',
+            smooth: true,
+            //stack: '~',
+            data: ajaxData[1][0],
+        },
+        {
+            name: '新增游客',
+            type: 'line',
+            smooth: true,
+            //stack: '~',
+            data: ajaxData[1][1],
+        }
+    ]
+};
+
+  myChart.setOption(option)
+  // 浏览器发生调整
+  window.addEventListener('resize', function () {
+    myChart.resize()
+  })
+  $('.line1 a').click(function(){
+    var _idx = $(this).index()
+    // console.log(_idx)
+    option.series[0].data = ajaxData[_idx][0]
+    option.series[1].data = ajaxData[_idx][1]
+    myChart.setOption(option)
+  })
+})();
+// 折线图2
+(function(select){
+  var myChart = echarts.init(document.querySelector('.line2 .chart'))
+  var ajaxData =  [
+    [Mock.mock({'data|30':[/^[1-2]\d{0,1}/]}).data, Mock.mock({'data|30':[/^[1-2]\d{0,1}/]}).data],
+    [Mock.mock({'data|30':[/^[1-2]\d{0,1}/]}).data, Mock.mock({'data|30':[/^[1-2]\d{0,1}/]}).data]
+  ]
+  var option = {
+    color: ['rgba(148,13,13,1)','rgba(34,193,195,1)'],
+    tooltip: {
+      trigger: 'axis'
+    },
+    grid: {
+      top:'30',
+      left: '10',
+      right: '10',
+      bottom: '10',
+      containLabel: true,
+    },
+    legend: {
+        textStyle: {
+          color: '#4c9bfd',
+          fontSize:12
+        },
+        top: '0'
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: [
+          '01','02','03','04','05','06','07','08','09','10',
+          '11','12','13','14','15','16','17','18','19','20',
+          '21','22','23','24','25','26','27','28','29','30',
+      ] ,
+        axisTick: {show: false},
+        splitLine: {show: false},
+        axisLine: {textStyle: {color: 'rgba(255,255,255,.1)'}},
+        axisLabel: {textStyle: {color: 'rgba(255,255,255,.5)'}}
+      },
+      yAxis: {
+        type: 'value',
+        splitLine: {lineStyle: {color: 'rgba(255,255,255,.1)'}},
+        axisTick: {show: false},
+        axisLine: {textStyle: {color: 'rgba(255,255,255,.1)'}},
+        axisLabel: {textStyle: {color: 'rgba(255,255,255,.5)'}}
+      },
+      series: [{
+          name: '播放量',
+          data: ajaxData[1][0],
+          type: 'line',
+          smooth: true,
+          showSymbol: false,
+          symbolSize:5,
+          lineStyle: {
+            width: 2,
+            color:'rgba(148,13,13,1)'
+          },
+          areaStyle: {
+            color: {
+              type: 'linear',
+              x: 0,y: 0,x2: 0,y2: 1,
+              colorStops: [{offset: 0, color: 'rgba(148,13,13,0.4)'}, { offset: 1, color: 'rgba(104,155,255,0.1)'}],
+              global: false // 缺省为 false
+            }
+          },
+          emphasis:{
+            itemStyle: {
+              color:'rgba(148,13,13,1)',
+              borderWidth:12,
+              borderColor:'rgba(221,220,107,.1)'
+            }
+          }
+      },{
+        name: '转发量',
+        data: ajaxData[1][1],
+        type: 'line',
+        smooth: true,
+        showSymbol: false,
+        symbolSize:5,
+        lineStyle: {
+          width: 2,
+          color:'rgba(34,193,195,1)'
+        },
+        areaStyle: {
+          color: {
+            type: 'linear',
+            x: 0,y: 0,x2: 0,y2: 1,
+            colorStops: [{offset: 0, color: 'rgba(34,193,195,.4)'}, { offset: 1, color: 'rgba(253,187,45,0.1)'}],
+            global: false // 缺省为 false
+          },
+        },
+        emphasis:{
+          itemStyle: {
+            color:'rgba(34,193,195,1)',
+            borderWidth:12,
+            borderColor:'rgba(221,220,107,.1)'
+          }
+        }
+    }]
+  };
+  myChart.setOption(option)
+  // 浏览器发生调整
+  window.addEventListener('resize', function () {
+    myChart.resize()
+  })
+  $('.line2 a').click(function(){
+    var _idx = $(this).index()
+    //alert(1)
+    console.log(_idx)
+    option.series[0].data = ajaxData[_idx][0]
+    option.series[1].data = ajaxData[_idx][1]
+    myChart.setOption(option)
+  })
+})();
+
+// $http.get('/abc').then(function (res) {
+//   console.log(res)
+// })
